50431fc3ffc8bc8f8016ec8c1c1f70d0ada25e19
Eric Widjaja <ericwidjaja@pursuit.org>
2019-09-24 19:03:01 -0400

    created VC and its labels, button, date picker

---
+++ UserDefaultsLab/UserDefaultsLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsLab
+//
+//  Created by Eric Widjaja on 9/24/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
