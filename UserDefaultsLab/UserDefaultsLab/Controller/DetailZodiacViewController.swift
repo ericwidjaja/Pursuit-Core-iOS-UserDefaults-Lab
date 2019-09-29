1fd265e05554dbcb275ddf9cf324eab063bc6e51
Eric Widjaja <ericwidjaja@pursuit.org>
2019-09-28 23:29:43 -0400

    connected TextField to firstVC, segue to detailVC

---
+++ UserDefaultsLab/UserDefaultsLab/Controller/DetailZodiacViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailZodiacViewController.swift
+//  UserDefaultsLab
+//
+//  Created by Eric Widjaja on 9/28/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class DetailZodiacViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UserDefaultsLab/UserDefaultsLab/Controller/FirstViewController.swift
+++ UserDefaultsLab/UserDefaultsLab/Controller/FirstViewController.swift
@@ -10,25 +10,66 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    //MARK: - Properties:
+    let userInput = UserDefaults.standard
     
-    
-    @IBOutlet weak var userBirthdate: UIDatePicker!
-    
-    
-    
-    
-
-    @IBAction func chosenBirthdate(_ sender: UIDatePicker) {
+    var userName = "" {
+        didSet {
+            userInput.set(userName, forKey: "userName")}
     }
     
+    var birthDay: [String] = [] {
+        didSet {
+            userInput.set(birthDay, forKey: "birthDay")}
+    }
+    
+    var birthDate: Date = Date() {
+        didSet {
+            userInput.set(birthDate, forKey: "birthDate")}
+    }
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var userBirthdate: UIDatePicker!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    //MARK: - IBAction
+
+    @IBAction func seeMyHoroscope(_ sender: UIButton) {
+//        getBirthDateFromDatePicker()
+//        getUserName()
+//        getUserZodiac()
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    //MARK: - Methods
+    private func getUserName() {
+        
+        
+    }
+    
 
 
+}
+extension FirstViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text, let textRange = Range(range, in: text) {
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            userName = updatedText
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        return true
+    }
 }
 
